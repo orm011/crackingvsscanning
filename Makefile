1eb9e34360d712daa44f4dab4626dfe032ec7768
Oscar R. Moll <rimoll@gmail.com>
2015-02-19 10:55:20 -0500

    memcpy bench

--- Makefile
+++ Makefile
@@ -94,3 +94,6 @@ predicatedInRegister2: outputdir
 
 simd: outputdir
 	gcc $(LDFLAGS) $(CFLAGS) -std=gnu99 -o bin/simd$(VECTORSIZE)Ints_$(DISTRIBUTION) -DVECTORSIZE=$(VECTORSIZE) -DDISTRIBUTION=$(DISTRIBUTION) -DSEED=$(SEED) -DSKEW=$(SKEW) Implementations/simd.c Framework/main.c Implementations/distributions.c Implementations/create_values.c $(LDFLAGS)
+
+memcpybench: outputdir
+	gcc $(CFLAGS) -std=gnu99 memcpybench.c -o bin/memcpybench
---
+++ memcpybench.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include <sys/time.h>
+#include <getopt.h>
+
+
+long timediff(struct timeval before, struct timeval after){
+  return (after.tv_usec - before.tv_usec) + (after.tv_sec-before.tv_sec)*1000000;
+}
+
+
+int main( int argc, char ** argv) {
+
+  long sizemb = -1;
+  char c = -1;
+
+  while ((c = getopt (argc, argv, "s:")) != -1) {
+    switch (c) {
+    case 's':
+      sizemb = atoi(optarg);
+      break;
+    default:
+      assert(("input error", 0));
+    }
+  }
+
+  assert(sizemb > 0);
+  size_t num = ((size_t)sizemb) * 1024 * 1024 / sizeof(char);
+  char * src = (char*)malloc(num);
+  char * dst = (char*)malloc(num);
+
+
+  struct timeval before, after;
+  gettimeofday(&before, NULL);
+  memmove(dst, src, num);
+  gettimeofday(&after, NULL);
+
+  long diff = timediff(before, after);
+  printf("{\"sizemb\": %llu, \"timemilli\": %llu}\n", sizemb, diff/1000);
+}
