ac5d151edab2bc32304f0f489b6808dedd67f9c9
Oscar R. Moll <rimoll@gmail.com>
2015-02-19 11:14:43 -0500

    added algorithm option

--- memcpybench.c
+++ memcpybench.c
@@ -15,29 +15,40 @@ long timediff(struct timeval before, struct timeval after){
 int main( int argc, char ** argv) {
 
   long sizemb = -1;
+  const char * algo;
   char c = -1;
 
-  while ((c = getopt (argc, argv, "s:")) != -1) {
+  while ((c = getopt (argc, argv, "s:a:")) != -1) {
     switch (c) {
     case 's':
       sizemb = atoi(optarg);
       break;
+    case 'a':
+      algo = optarg;
+      break;
     default:
       assert(("input error", 0));
-    }
-  }
+    }  }
 
   assert(sizemb > 0);
-  size_t num = ((size_t)sizemb) * 1024 * 1024 / sizeof(char);
-  char * src = (char*)malloc(num);
-  char * dst = (char*)malloc(num);
 
+  size_t num = ((size_t)sizemb) * 1024 * 1024 / sizeof(char);
+  const char * src = (char*)malloc(num);
+  char * dst = (char*)malloc(num);
 
   struct timeval before, after;
   gettimeofday(&before, NULL);
-  memmove(dst, src, num);
+
+  if (strcmp(algo,"memmove") == 0 ) {
+    memmove(dst, src, num);
+  } else if (strcmp(algo, "memcpy" ) == 0) {
+    memcpy(dst, src, num);
+  } else {
+    assert(("invalid algo", 0));
+  }
+
   gettimeofday(&after, NULL);
 
   long diff = timediff(before, after);
-  printf("{\"sizemb\": %lld, \"timemilli\": %lld}\n", sizemb, diff/1000);
+  printf("{\"sizemb\": %ld, \"timemilli\": %ld}\n", sizemb, diff/1000);
 }
