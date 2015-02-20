c81ea95f4c395f417da8a50a62daf27be764211f
Oscar R. Moll <rimoll@gmail.com>
2015-02-20 17:17:30 -0500

    libc machine summary improvement

--- machine_summary.sh
+++ machine_summary.sh
@@ -3,14 +3,15 @@ CPUCNT=$(cat /proc/cpuinfo | grep processor | wc -l)
 MEM=$(free -g | head -n 2 | tail -n 1 | awk '{print $2}')
 GCC=$(gcc --version | grep gcc)
 KER=$(uname -r)
-LIBCVER=$(/lib64/libc.so.6 | grep 'release version')
-LIBCCMP=$(/lib64/libc.so.6 | grep 'Compiled by')
-LIBCKER=$(/lib64/libc.so.6 | grep 'Compiled on')
+
+LIBC=$(ldd /usr/bin/ld | grep libc | sed 's/.* => \(\/[^ ]*\) .*/\1/')
+LIBCVER=$($LIBC | grep 'release version')
+PAGESIZE=$(getconf PAGESIZE)
+
 echo cpu $CPU 
-echo cpus: $CPUCNT
+echo threads: $CPUCNT
 echo ram GB: $MEM 
 echo kernel: $KER
 echo gcc version: $GCC 
 echo libc version: $LIBCVER
-echo libc built: $LIBCCMP
-echo libc kernel: $LIBCKER
+echo page size: $PAGESIZE
