e640dfc70d39b578e7e86ea2cd1bc7f25ad5ada6
Oscar R. Moll <rimoll@gmail.com>
2015-02-26 00:53:57 -0500

    fix linesize

--- Implementations/distributions.c
+++ Implementations/distributions.c
@@ -23,7 +23,7 @@ randomDistribution(targetType *buffer, unsigned int size, targetType domain, int
 {
 	unsigned int p = 0, q = size;
 	unsigned int* rbuf = NULL;
-	size_t linesize = 64;
+	const size_t linesize = 64;
 	int r= posix_memalign((void**)&rbuf, linesize, omp_get_max_threads()*linesize);
 	assert(r == 0);
 
@@ -35,7 +35,7 @@ randomDistribution(targetType *buffer, unsigned int size, targetType domain, int
 
 	#pragma omp parallel for
 	for (int i=0; i < q; i++)
-		buffer[i] = rand_r(rbuf+omp_get_thread_num()*linesize);
+		buffer[i] = rand_r(rbuf+omp_get_thread_num()*(linesize/sizeof(unsigned int)));
 }
 
 void
