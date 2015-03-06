20be9219ae45f929eda7a03bb18fe5a57fa935c6
Oscar Moll <orm@istc2.(none)>
2015-03-06 15:11:58 -0500

    Revert "try openmp for"
    This reverts commit 0fafe9c2747ed7cac6d3b433232b703566ffbd6e.

--- Implementations/scanning.c
+++ Implementations/scanning.c
@@ -3,15 +3,14 @@
 
 targetType* performCrack(targetType* buffer, targetType* payloadBuffer, size_t bufferSize, targetType pivot, const targetType pivot_P) {
 	targetType* result = (targetType*)malloc(sizeof(targetType)*bufferSize);
+#pragma omp parallel
 	{
-//		size_t i = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
-//		size_t end = (bufferSize*(omp_get_thread_num()+1))/omp_get_num_threads();
-//		size_t outI = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
-	#pragma omp parallel for
-		for (size_t i = 0; i < bufferSize; i+=64) {
-			for (int j = 0; j < 64; j++) {
-				result[i] += buffer[i + j] < pivot;
-			}
+		size_t i = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
+		size_t end = (bufferSize*(omp_get_thread_num()+1))/omp_get_num_threads();
+		size_t outI = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
+		for (; i < end; i+=64){
+			for (int j = 0; j < 64; j++)
+				result[outI] += buffer[i + j] < pivot;
 		}
 	}
 
