893a6c05852a73e08a66fa3cddb7d73c2f7f5c5a
Oscar R. Moll <rimoll@gmail.com>
2015-03-09 03:21:23 -0400

    copying

--- Implementations/copying.cc
+++ Implementations/copying.cc
@@ -1,17 +1,10 @@
 #include "../Framework/interface.h"
-#include <omp.h>
 
 targetType* performCrack(targetType* buffer, targetType* payloadBuffer, size_t bufferSize, targetType pivot, const targetType pivot_P) {
 	targetType* result = (targetType*)malloc(sizeof(targetType)*bufferSize);
-#pragma omp parallel
-	{
-		size_t i = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
-		size_t end = (bufferSize*(omp_get_thread_num()+1))/omp_get_num_threads();
-		size_t outI = (bufferSize*omp_get_thread_num())/omp_get_num_threads();
-		for (; i < end; i++){
-			result[outI] = buffer[i];
-			++outI;
-		}
+
+	_Cilk_for (int i = 0; i < bufferSize; ++i) {
+		result[i] = buffer[i];
 	}
 
 	return result;
